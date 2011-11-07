e5327fdc9984834d11e8ff1adc596b1686634af7
Bryan Davis <bd808@bd808.com>
2011-11-07 14:10:00 -0700

    anatomy

--- anatomy/index.php
+++ anatomy/index.php
@@ -7,8 +7,17 @@
     <?php include "../header.php";?>
     <br/><br/><br/>
     <div class="container">
-      <img src="../img/anatomy1.jpg"/>
-      <img src="../img/anatomy2.jpg"/>
+      <h1>Anatomy</h1>
+      <p><img src="../img/anatomy1.jpg"/></p>
+      <p><img src="../img/anatomy2.jpg"/></p>
+      <h2>General Characteristics</h2>
+      <dl>
+        <dt>Height</dt><dd>5' to 7' tall</dd>
+        <dt>Weight</dt><dd>600 to 1200 lbs</dd>
+        <dt>Average Lifespan</dt><dd>approx 1,000 yrs</dd>
+        <dt>Horns</dt><dd>One awesome horn on forehead!</dd>
+      </dl>
+      <p>Unicorns breathe through their eyelids because they are awesome.</p>
     </div>
     <?php include "../footer.php";?>
   </body>
