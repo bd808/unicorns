53596b04cb22478263be56cfdbbc90dad69c0050
Bryan Davis <bd808@bd808.com>
2011-11-08 09:40:36 -0700

    habitat

--- habitat/index.php
+++ habitat/index.php
@@ -1,5 +1,50 @@
 <?php require_once '../base.php'; ?>
 <?php startblock('content'); ?>
 <h1>Habitat</h1>
-<p>coming soon</p>
+<p>Typically, unicorns live deep in the forest and are rarely seen by people.
+Unicorns, like other walking creatures, live on the ground and do not
+choose to reside in caves and certainly not in trees. Although many people
+believe that unicorns primarily live in Europe, they are much more
+widespread on the Earth. There are numerous unicorn traditions that
+describe where they live and, like people, the appearance and even some
+behaviors of unicorns differ depending on where they make their home.
+Regardless of this, the natural surroundings for unicorns are heavily
+wooded areas, though they may live near a seashore or occasionally in
+mountainous areas. All of these locations must be remote and far from human
+towns and cities.
+
+<p>Nations that have confirmed communities of unicorns are Germany, China,
+Japan, Persia, and Vietnam. In China, the unicorn is one of four sacred
+animals that are the guardians of the hidden realms on the earth and places
+beyond our own existence. Chinese unicorns are rarely seen by men or
+associated with a specific region since they primarily exist other worldly
+domains.
+
+<p>In Germany, the legend of the Einhorn is associated with cult worship of
+the adoration of the virgin. The Harz mountain region is where unicorns are
+most likely to be found. In fact, there is a cave in this area which to
+this day is called the Einhornhohle. It acquired its name from a story
+which took place in the days when much of Germany was covered with dark,
+unmapped forests, which were ruled by the old gods.
+
+<p>Japanese unicorns can often take the form of the legendary Kirin. Like
+unicorns in other regions, they are shy and rarely seen and thus are not
+associated with a particular area in Japan.
+
+<p>Unicorns in ancient Persia are thought to be extinct but were considered
+more fierce and ferocius than those living in other parts of the world. It
+was thought that a Persian unicorn had the strength to kill even an
+elephant. This unicorn inhabited the grassy plains and deserts of India and
+Persia, perhaps even extending to northeastern Africa, and whenever it
+appeared it was greatly respected for its enormous strength and ferocity.
+
+<p>Little is known about the unicorns in Vietnam. What we know is primarily
+communicated by custom and ritual. In Vietnam, a yearly unicorn dance is
+performed. The ritual occurs on the full moon of the eighth month, which is
+the beginning of the monsoon season.
+
+<p>The dance looks similar to the Chinese dragon dances. People put on masks
+and costumes to conceal their identity. They tie an effigy of a unicorn up
+onto a platform. Archers then shoot at it and sing the effigy song to
+herald the rains.
 <?php endblock('content'); ?>
